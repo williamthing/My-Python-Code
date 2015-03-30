b6e750959c473ee0a09a4d6ab474c867cf66e0b0
williamthing <williamthing@gmail.com>
2015-03-29 23:41:38 -0700

    just testing

---
+++ Butterfly/Butterfly.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# William Thing
+#
+# Butterfly
+#
+
+def main():
+	count = 1
+	while count <= 10:
+		print 'Butterfly', count
+		print 'testing...'
+		count += 1
+
+if __name__ == "__main__": main()
