9eb5b786c86b3a458f250b16ccda112b2350e2b5
williamthing <williamthing@gmail.com>
2015-04-17 22:36:20 -0700

    part 1 done

--- Django/mysite/polls/models.py
+++ Django/mysite/polls/models.py
@@ -7,16 +7,28 @@
 # Creates a Python database-access API for accessing Question
 # and Choice objects.
 
+import datetime
+
 from django.db import models
+from django.utils import timezone
 
 class Question(models.Model):
 	question_text = models.CharField(max_length = 200)
 	pub_date = models.DateTimeField('date published')
 
+	def was_published_recently(self):
+		return self.pub_date >= timezone.now() - datetime.timedelta(days = 1)
+
+	def __str__(self):
+		return self.question_text
+
 class Choice(models.Model):
 	question = models.ForeignKey(Question)				# each Choice is related to a single Question
 	choice_text = models.CharField(max_length = 200)
 	votes = models.IntegerField(default = 0)
+
+	def __str__(self):
+			return self.choice_text
 
 
 
