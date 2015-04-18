83f0d11da2e61e77f72d71b3900a9d7a698fa500
williamthing <williamthing@gmail.com>
2015-04-17 21:55:03 -0700

    update

--- Django/mysite/polls/models.py
+++ Django/mysite/polls/models.py
@@ -3,17 +3,19 @@
 # First Django Web App!!!
 # Poll App
 #
-# 
+# Creates a database schema
+# Creates a Python database-access API for accessing Question
+# and Choice objects.
 
 from django.db import models
 
 class Question(models.Model):
-	question_text = models.charField(max_length = 200)
+	question_text = models.CharField(max_length = 200)
 	pub_date = models.DateTimeField('date published')
 
 class Choice(models.Model):
 	question = models.ForeignKey(Question)				# each Choice is related to a single Question
-	choice_text = models.charField(max_length = 200)
+	choice_text = models.CharField(max_length = 200)
 	votes = models.IntegerField(default = 0)
 
 
