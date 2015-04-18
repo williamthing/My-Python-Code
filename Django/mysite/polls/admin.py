dc75e51edaf43b04a2a53e7d29c5420bdd14eac7
williamthing <williamthing@gmail.com>
2015-04-18 12:57:54 -0700

    polls app continued

--- Django/mysite/polls/admin.py
+++ Django/mysite/polls/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 
-from .models import Question, Choice
+from .models import Choice, Question
+
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
 
 
 class QuestionAdmin(admin.ModelAdmin):
@@ -8,7 +13,7 @@ class QuestionAdmin(admin.ModelAdmin):
         (None,               {'fields': ['question_text']}),
         ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
     ]
+    inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
 
 admin.site.register(Question, QuestionAdmin)
-
-admin.site.register(Choice)
