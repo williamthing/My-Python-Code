9e982b2795fb108bb283e876a1ac2a00708b5a88
williamthing <williamthing@gmail.com>
2015-04-19 12:31:17 -0700

    polls webapp

--- Django/mysite/polls/models.py
+++ Django/mysite/polls/models.py
@@ -17,7 +17,10 @@ class Question(models.Model):
 	pub_date = models.DateTimeField('date published')
 
 	def was_published_recently(self):
-		return self.pub_date >= timezone.now() - datetime.timedelta(days = 1)
+        return self.pub_date >= timezone.now() - datetime.timedelta(days = 1)
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
 
 	def __str__(self):
 		return self.question_text
