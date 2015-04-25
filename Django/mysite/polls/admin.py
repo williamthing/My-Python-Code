b1202b823c9509a65948ad78e61718af54f3fd78
williamthing <williamthing@gmail.com>
2015-04-24 23:25:57 -0700

    server running smoothly

--- Django/mysite/polls/admin.py
+++ Django/mysite/polls/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
 
-from .models import Question
+from .models import Choice, Question
 
-admin.site.register(Question)
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
