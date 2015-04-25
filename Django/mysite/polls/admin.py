22e4a365aa83e3144b3c383508c225f527a40418
williamthing <williamthing@gmail.com>
2015-04-24 22:36:46 -0700

    django part1 up

--- Django/mysite/polls/admin.py
+++ Django/mysite/polls/admin.py
@@ -1,3 +1,10 @@
-from django.contrib import admin
+import datetime
 
-# Register your models here.
+from django.db import models
+from django.utils import timezone
+
+
+class Question(models.Model):
+    # ...
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
